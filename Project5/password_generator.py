ff77ea9c3adef0a42194734a669107c9ce7f156c
Aaron L. Hevia <59777031+AaronHevia@users.noreply.github.com>
2021-12-21 13:39:33 -0500

    Add files via upload

---
+++ Project5/password_generator.py
@@ -0,0 +1,27 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+number_of_letters = int(input("How many letters would you like in your password?\n"))
+number_of_numbers = int(input("How many numbers would you like in your password?\n"))
+number_of_characters = int(input("How many special characters would you like in your password?\n"))
+password = []
+
+while number_of_letters > 0 or number_of_numbers > 0 or number_of_characters > 0:
+    choice = random.randint(0, 2)
+    if number_of_letters > 0 and choice == 0:
+        password.append(random.choice(letters))
+        number_of_letters -= 1
+    elif number_of_numbers > 0 and choice == 1:
+        password.append(random.choice(numbers))
+        number_of_numbers -= 1
+    elif number_of_characters > 0 and choice == 2:
+        password.append(random.choice(symbols))
+        number_of_characters -= 1
+prompt = "".join(password)
+print(f"Here is your password: {prompt}")
