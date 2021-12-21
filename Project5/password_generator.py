6320f4c5a3ac26071ca86d0bf653609436c52284
Aaron L. Hevia <59777031+AaronHevia@users.noreply.github.com>
2021-12-21 13:54:29 -0500

    Update password_generator.py

--- Project5/password_generator.py
+++ Project5/password_generator.py
@@ -10,18 +10,22 @@ print("Welcome to the PyPassword Generator!")
 number_of_letters = int(input("How many letters would you like in your password?\n"))
 number_of_numbers = int(input("How many numbers would you like in your password?\n"))
 number_of_characters = int(input("How many special characters would you like in your password?\n"))
+total  = number_of_letters + number_of_numbers + numbe_of_characters
 password = ""
 
-while number_of_letters > 0 or number_of_numbers > 0 or number_of_characters > 0:
+while total > 0:
     choice = random.randint(0, 2)
     if number_of_letters > 0 and choice == 0:
         password += random.choice(letters)
+        total -= 1
         number_of_letters -= 1
     elif number_of_numbers > 0 and choice == 1:
         password += random.choice(numbers)
+        total -= 1
         number_of_numbers -= 1
     elif number_of_characters > 0 and choice == 2:
         password += random.choice(symbols)
+        total -= 1
         number_of_characters -= 1
 
 print(f"Here is your password: {password}")
