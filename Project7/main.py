4d7a027481dabdc0e16105474e3b96cc3686e6c4
Aaron L. Hevia <59777031+AaronHevia@users.noreply.github.com>
2021-12-23 14:16:18 -0500

    Add files via upload

---
+++ Project7/main.py
@@ -0,0 +1,60 @@
+import os
+import random
+import hangman_dictionary as dictionary
+import hangman_art as art
+
+
+def clear_console():
+    if os.name in ('ce', 'nt', 'dos'):
+        os.system('cls')
+    elif os.name in ('linux', 'osx', 'posix'):
+        os.system('clear')
+
+
+chosen_word = random.choice(dictionary.word_list)
+
+display = []
+guessed_letters = []
+lives = 6
+
+for letter in chosen_word:
+    display.append("_")
+
+game_over = False
+joined_display = "".join(display)
+
+print(art.logo)
+
+while not game_over:
+    print(art.lives[lives])
+    print(joined_display)
+    guess = input("Guess a letter:  ").lower()
+    clear_console()
+
+    if guess in guessed_letters:
+        print(f"You have already guessed the letter {guess}.  Try again.")
+    else:
+        guessed_letters.append(guess)
+
+        for position in range(len(chosen_word)):
+            if guess == chosen_word[position]:
+                display[position] = guess
+
+        joined_display = "".join(display)
+
+        if guess not in chosen_word:
+            lives -= 1
+            print(f'"{guess}" is not in the word.  You lose a life.')
+
+        if joined_display == chosen_word or lives == 0:
+            game_over = True
+
+if lives > 0:
+    print(joined_display)
+    print("You win.")
+else:
+    print(art.lives[0])
+    print(f"The word was {chosen_word}.")
+    print("You lose.")
+
+x = input('press "x" to exit')
