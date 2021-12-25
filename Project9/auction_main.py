ba9ac9c9fb56fb5372a45d10044700432063c1fa
Aaron L. Hevia <59777031+AaronHevia@users.noreply.github.com>
2021-12-25 13:25:44 -0500

    Add files via upload

---
+++ Project9/auction_main.py
@@ -0,0 +1,49 @@
+import os
+from auction_art import logo
+
+
+def clear_console():
+    if os.name in ('ce', 'nt', 'dos'):
+        os.system('cls')
+    elif os.name in ('linux', 'osx', 'posix'):
+        os.system('clear')
+
+
+bids = []
+
+
+def add_bid(bidder, bid):
+    bid = {
+        "bidder": bidder,
+        "bid": bid
+    }
+    bids.append(bid)
+
+
+def winning_bid():
+    leading_bid = 0
+    winner = ""
+    for bid in bids:
+        if bid["bid"] > leading_bid:
+            leading_bid = bid["bid"]
+            winner = bid["bidder"]
+    print(f"The winner is {winner} with a bid of ${leading_bid}.")
+
+
+def blind_bid():
+    open_bids = True
+    while open_bids:
+        name = input("What is your name?:  ")
+        bid = int(input("What is your bid?:  $"))
+        add_bid(name, bid)
+        more_bids = input("Are there any other bidders? Type 'yes' or 'no'.  ").lower()
+        clear_console()
+        if more_bids == "no":
+            open_bids = False
+            winning_bid()
+
+
+print(logo)
+print("Welcome to the secret auction program.")
+blind_bid()
+x = input('press "x" to exit')
