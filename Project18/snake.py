ed53d43c126dc579b1ae4bc339a094bdf2d33039
Aaron L. Hevia <aaron.hevia@gmail.com>
2022-01-05 12:12:17 -0500

    Updated snake creation and movement for OOP.

---
+++ Project18/snake.py
@@ -0,0 +1,56 @@
+from turtle import Turtle
+
+STARTING_X_POSITIONS = [0, -20, -40]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.body = []
+        self.create_snake()
+        self.head = self.body[0]
+
+    def create_snake(self):
+        """Creates the starting body for the snake."""
+        for x in STARTING_X_POSITIONS:
+            segment = Turtle('square')
+            segment.penup()
+            segment.color("white")
+            segment.setx(x)
+            self.body.append(segment)
+
+    def move(self):
+        """Moves the snake towards the direction of the head."""
+        for part in range(len(self.body) - 1, 0, -1):
+            # if part == snake[0]:
+            #     on_key_move(snake[part])
+            #     snake[part].forward(10)
+            # else:
+            front_x = self.body[part - 1].xcor()
+            front_y = self.body[part - 1].ycor()
+            self.body[part].goto(front_x, front_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def move_right(self):
+        """Moves the head of the snake right if it is not facing left."""
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def move_up(self):
+        """Moves the head of the snake up if it is not facing down."""
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def move_left(self):
+        """Moves the head of the snake left if it is not facing right."""
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def move_down(self):
+        """Moves the head of the snake down if it is not facing up."""
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
--- Project18/snake_main.py
+++ Project18/snake_main.py
@@ -1,4 +1,7 @@
 from turtle import Screen, Turtle
+from snake import Snake
+import scoreboard
+import food
 import time
 
 # Set playing area.
@@ -10,67 +13,28 @@ screen.title('My snake game.')
 screen.tracer(0)
 # Get input from user
 screen.listen()
+# Create starting snake.
+snake = Snake()
 
 
-# Create starting snake body.
-def create_body():
-    """
-    Creates the starting body for the snake.
-    @return:  Returns the starting body.
-    """
-    x_position = [0, -20, -40]
-    body = []
-    for x in x_position:
-        segment = Turtle('square')
-        segment.penup()
-        segment.color("white")
-        segment.setx(x)
-        body.append(segment)
-    return body
+# Create snake control function
+def on_key_move():
+    """Triggers associated movement when the respective key is pushed."""
+    screen.onkey(snake.move_up, "Up")
+    screen.onkey(snake.move_down, "Down")
+    screen.onkey(snake.move_left, "Left")
+    screen.onkey(snake.move_right, "Right")
 
 
-snake = create_body()
-
-
-# TODO:  Control the snake with key presses.
-def on_key_move(element):
-    def move_right(head):
-        head.setheading(0)
-
-    def move_up(head):
-        head.setheading(90)
-
-    def move_left(head):
-        head.setheading(180)
-
-    def move_down(head):
-        head.setheading(270)
-
-    screen.onkey(move_up, "w")
-    screen.onkey(move_down, "s")
-    screen.onkey(move_left, "a")
-    screen.onkey(move_right, "d")
-
-
-# TODO:  Move the snake.
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.25)
-
-    for part in range(len(snake) - 1, 0, -1):
-        # if part == snake[0]:
-        #     on_key_move(snake[part])
-        #     snake[part].forward(10)
-        # else:
-        front_x = snake[part - 1].xcor()
-        front_y = snake[part - 1].ycor()
-        snake[part].goto(front_x, front_y)
-
-    snake[0].forward(20)
-
-
+    # Move the snake
+    snake.move()
+    # Control the snake with key presses.
+    on_key_move()
 
 
 # TODO:  Detect collision with food.
