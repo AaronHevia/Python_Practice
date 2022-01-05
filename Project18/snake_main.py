ca456b2b895bfb2d8b54672f2d46fb78d8872b4b
Aaron L. Hevia <aaron.hevia@gmail.com>
2022-01-05 06:32:56 -0500

    Created Project 18 folder and initial TODO tasks.

---
+++ Project18/snake_main.py
@@ -0,0 +1,16 @@
+# TODO:  Create Snake body.
+
+# TODO:  Move the snake.
+
+# TODO:  Control the snake with key presses.
+
+# TODO:  Detect collision with food.
+
+# TODO:  Create a scoreboard.
+
+# TODO:  Determine game over:
+ # TODO:  Detect collision with wall.
+
+ # TODO:  Detect collision with tail.
+
+
