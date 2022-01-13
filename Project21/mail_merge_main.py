93eadc6efc4263a6e2d7327aa261515647ddafad
Aaron L. Hevia <aaron.hevia@gmail.com>
2022-01-13 18:49:37 -0500

    Completed Project 21 workflow.

--- Project21/mail_merge_main.py
+++ Project21/mail_merge_main.py
@@ -1,8 +1,18 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# Creates a letter using starting_letter.txt for each name in invited_names.txt
+NAME_HOLDER = '[name]'
+
+with open('./Input/Names/invited_names.txt', 'r') as name_data:
+    names = name_data.readlines()
+for name in range(len(names)):
+    names[name] = names[name].strip('\n')
+
+# Create a template letter to print.
+with open('./Input/Letters/starting_letter.txt', 'r') as letter:
+    template = letter.read()
+
+# Save the letters in the folder "ReadyToSend".
+for name in range(len(names)):
+    # Replace the [name] placeholder with the actual name.
+    draft = template.replace(NAME_HOLDER, names[name])
+    with open(f'./Output/ReadyToSend/letter_for_{names[name]}.txt', 'w') as final_letter:
+        final_letter.write(draft)
