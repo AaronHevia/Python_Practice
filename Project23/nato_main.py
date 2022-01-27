8aeae25c16af0f5e798dcb334fa80ef54ea46e2c
Aaron L. Hevia <aaron.hevia@gmail.com>
2022-01-27 15:52:44 -0500

    Added input handling.

--- Project23/nato_main.py
+++ Project23/nato_main.py
@@ -4,8 +4,17 @@ import pandas
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-# Create a list of the phonetic code words from a word that the user inputs.
-word = input('Enter a word to spell in NATO format:  ').upper()
-nato_spelled = [nato_dict[letter] for letter in list(word)]
 
-print(nato_spelled)
+# Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    word = input('Enter a word to spell in NATO format:  ').upper()
+    try:
+        nato_spelled = [nato_dict[letter] for letter in list(word)]
+    except KeyError:
+        print('Please spell the word with letters in the alphabet.')
+        generate_phonetic()
+    else:
+        print(nato_spelled)
+
+
+generate_phonetic()
